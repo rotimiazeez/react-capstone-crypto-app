5826721b36bb336c9798217c2161261341798042
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-10-14 09:56:57 +0100

    chore: Createa new app, install all dependencies and set-up linter config

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+}
+
+export default App;
