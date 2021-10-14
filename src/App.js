d409011299f0f054a8ea52d794dbdd743bb76dc6
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-10-14 21:27:16 +0100

    style: Style page to fit the behance template

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
-// import logo from './logo.svg';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { Switch, Route, HashRouter as Router } from 'react-router-dom';
+import MainPage from './Components/MainPage';
+import Details from './Components/Details';
+import { loadCrypto } from './redux/reducers/cryptoReducers';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadCrypto());
+  }, []);
 
+  return (
+    <div>
+      <Router basename="/">
+        <Switch>
+          <Route exact path="/">
+            <MainPage />
+          </Route>
+        </Switch>
+        <Details />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,20 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-// import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
-import store from './redux/store';
+import store from './redux/configureStore';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
-  </Provider>,
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
