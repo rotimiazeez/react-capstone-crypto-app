5ecdb072ae2908079aa3c0ac3a942ed0ab43e776
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-10-15 17:05:47 +0100

    fix: Fix eslint error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+/* eslint-disable import/extensions */
 import React, { useEffect } from 'react';
 import './App.css';
 import { useDispatch } from 'react-redux';
 import { Switch, Route, HashRouter as Router } from 'react-router-dom';
-import MainPage from './Components/MainPage';
-import Details from './Components/Details';
+import { MainPage } from './Components/MainPage';
+import { Details } from './Components/Details';
 import { loadCrypto } from './redux/reducers/cryptoReducers';
 
 function App() {
