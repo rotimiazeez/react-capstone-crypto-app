d1bf1c997bf85b85e18af3090e2b60f9eea347fc
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-10-15 16:54:45 +0100

    fix: Fix extension error

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { useDispatch } from 'react-redux';
 import { Switch, Route, HashRouter as Router } from 'react-router-dom';
-import MainPage from './Components/MainPage';
-import Details from './Components/Details';
+import MainPage from './Components/MainPage.js';
+import Details from './Components/Details.js';
 import { loadCrypto } from './redux/reducers/cryptoReducers';
 
 function App() {
