c4a4d96961b3b043906e9f880225b0aeab91bf98
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-10-15 17:01:04 +0100

    fix: Fix error

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { useDispatch } from 'react-redux';
 import { Switch, Route, HashRouter as Router } from 'react-router-dom';
-import MainPage from './Components/MainPage.js';
-import Details from './Components/Details.js';
+import MainPage from './Components/MainPage';
+import Details from './Components/Details';
 import { loadCrypto } from './redux/reducers/cryptoReducers';
 
 function App() {
