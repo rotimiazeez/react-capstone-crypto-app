370cdf71aab95b9aa74a7751b06045034a4e38c7
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-10-16 11:44:34 +0100

    fix: Add build to develop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-/* eslint-disable import/extensions */
 import React, { useEffect } from 'react';
 import './App.css';
 import { useDispatch } from 'react-redux';
 import { Switch, Route, HashRouter as Router } from 'react-router-dom';
-import MainPage from './Components/MainPage';
-import Details from './Components/Details';
+import MainPage from './components/MainPage';
+import Details from './components/Details';
 import { loadCrypto } from './redux/reducers/cryptoReducers';
 
 const App = () => {
